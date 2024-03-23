b2553928d4deffafbc70720e0ae3e8cdbf908208
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-03-23 15:53:12 +0545

    feat: improved stow script

--- windows/stow.ps1
+++ windows/stow.ps1
@@ -1,15 +1,44 @@
 param(
-    [string]$Target,
+    [string]$TargetPath,
     [string]$LinkPath
 )
 
-# Check if the original file path exists
-if (-not (Test-Path -Path $Target -PathType Leaf)) {
-    Write-Host "Error: Targeted file path '$Target' does not exist."
+
+# First check if $LinkPath exist or not
+if (-not $TargetPath) {
+    Write-Host "Error: TargetPath is required."
     exit
 }
 
-# Create symbolic link
-New-Item -ItemType SymbolicLink -Path $LinkPath -Target $Target
+# check if $LinkPath exist or not
+if (-not $LinkPath) {
+    Write-Host "Error: LinkPath is required."
+    exit
+}
 
-Write-Host "Symbolic link created successfully at '$LinkPath' pointing to '$Target'."
+$Target = Resolve-Path $TargetPath
+
+# Check if the Targetd file path exists
+if (-not (Test-Path -Path $Target -PathType Leaf)) {
+    Write-Host "Error: Targeted file '$Target' does not exist."
+    exit
+}
+
+# Get the filename from the $Target
+$FileName = Split-Path -Path $Target -Leaf
+echo $FileName
+
+$LinkPathLeaf = Split-Path -Path $LinkPath -Leaf
+echo $LinkPathLeaf
+
+if (-not ($LinkPathLeaf -eq $FileName)){
+  # $LinkPath doesn't contain the filename
+  # Create symbolic link
+  New-Item -ItemType SymbolicLink -Path $LinkPath\$FileName -Target $Target
+}
+else{
+  # $LinkPath contains the filename
+  # Create symbolic link
+  New-Item -ItemType SymbolicLink -Path $LinkPath -Target $Target
+}
+
